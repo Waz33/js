19ef6f5cbf5e1ecec7767b5b1dbbe7b6910d967e
wassilva <wassilva@prestadores.amil.com.br>
2018-12-07 18:32:56 -0200

    fundamentos from job

---
+++ hoisting.js
@@ -0,0 +1,16 @@
+
+//Hoisting = Icamento
+
+
+//Faz icamento
+console.log('a = ',a)
+var a = 2
+console.log('a = ',a)
+
+//Nao faz icamento
+console.log('b = ',b)
+let b = 4
+console.log('b = ',b)
+
+
+
